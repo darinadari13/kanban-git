0562c869c455de201d81279e28b8184178447a06
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-04-20 18:49:56 +0300

    api

--- src/services/API.tsx
+++ src/services/API.tsx
@@ -1,7 +1,42 @@
 import { Octokit } from "octokit";
-
-const octokit = new Octokit({ 
-  auth: 'YOUR-TOKEN',
-});
+import { useEffect, useState } from 'react';
 
 const PERSONAL_TOKEN = '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';
+// const repoUrl = 'https://github.com/facebook/react';
+
+
+export const RepoIssues = ({ repoUrl }: {repoUrl: string}) => {
+
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    const octokit = new Octokit({ 
+      auth: PERSONAL_TOKEN,
+    });
+
+  const fetchIssues = async () => {
+      try {
+        const [owner, repo] = repoUrl.replace('https://github.com/', '').split('/');
+
+        const response = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+          owner,
+          repo,
+        });        
+        if (response.status === 200) {
+          console.log(response);
+          setIssues(response.data);
+        } else {
+          console.error('Failed to fetch issues:', response.statusText);
+        }
+      } catch (error) {
+          console.error('Failed to fetch issues:', error);
+      }
+    }
+    fetchIssues();
+  },[repoUrl])
+}
+
+
+
+
+
